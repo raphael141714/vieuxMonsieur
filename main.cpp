c957a0eb4c14f564f01b416cd735f8b636fa1aa5
raphael141714 <raphael141714@github.com>
2019-03-24 18:13:45 +0100

    push initial

---
+++ main.cpp
@@ -0,0 +1,76 @@
+/** ========================= **
+ *  Auteur: Mathieu Gardette
+ *  Promotion INFRES 10
+ ** ========================= **/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+#define DEBUG 
+
+using namespace std;
+
+class Cell {
+  protected :
+    int index;
+    int value;
+  public :
+    int getIndex() {return index;}
+    int getValue() {return value;}
+    void setValue(int newValue) {value = newValue;}
+    void setIndex(int newIndex) {index = newIndex;}
+};
+
+bool myfunction (Cell i,Cell j) { return (i.getValue() > j.getValue()); }
+
+//--- MAIN ---
+int main (int argc, char* argv[]){
+
+  int size;
+  vector<vector<Cell> > sorted ;
+  FILE *file;
+  file = fopen("./aff1.txt","r+");
+
+  fscanf(file, "%d", &size);
+
+  // Créer un tableau de "size" par "size" éléments de valeur -1
+  vector<vector<int> > arr (size, vector<int>(size, -1));
+
+  for (int i = 0; i<size; i++){
+    for (int j = 0; j <size; j++){
+      fscanf(file, "%d", &arr[i][j]);
+    }
+  }
+
+  //Remonte un vector rerésentant les colonnes d'une ligne de la matrice après l'avoir trié dans un vector de vector représentant la collection des lignes
+  //Les valeurs de la première colonne sont donc les maximums
+  for (int i=0; i<arr.size(); i++){
+
+    vector<Cell> line; // Chaque i est une ligne
+    for (int a = 0; a<arr[i].size(); a++) {
+      Cell item;
+      item.setValue(arr[i][a]);
+      item.setIndex(a);
+
+      line.push_back(item);
+    }
+
+    sort(line.begin(), line.end(), myfunction); 
+    sorted.push_back(line);
+  }
+
+  for (int a = 0; a < sorted.size(); a++) {
+    for (int b = 0; b < sorted[a].size(); b++) {
+      printf("%4d(%d) ", sorted[a][b].getValue(), sorted[a][b].getIndex());
+    }
+    printf("\n");
+  }
+
+  puts("\n");
+  fclose(file); 
+  return 0;
+}
